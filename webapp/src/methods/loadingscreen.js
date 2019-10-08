ea7258af129a586085aa41648e4986370fd8c8a8
Philipp Braun <philipp.braun@telekom.de>
2019-10-08 16:40:12 +0200

    loadingscreen centered

--- webapp/src/methods/loadingscreen.js
+++ webapp/src/methods/loadingscreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function notAuthenticated () {
 	return (
-		<div className="lds" style={{ left: '48%', top: '45%', position: 'absolute' }}>
+		<div className="lds" style={{ left: '48%', top: '48%', position: 'absolute' }}>
 			<div />
 			<div />
 			<div />
