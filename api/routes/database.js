a44f974cc94378a622925e0826f8cd2b36250e2a
Philipp Braun <philipp.braun@telekom.de>
2019-10-08 14:08:15 +0200

    changed Error handling

--- api/routes/database.js
+++ api/routes/database.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 //Connection Infos for DB change if need be
 var connection = mysql.createPool({
-	host     : process.env.DB_HOST || 'localhost',
-	user     : process.env.DB_USER || 'root',
-	password : process.env.DB_PASSWORD || 'root',
-	database : process.env.DB_DATABASE_NAME || 'csr_db',
-	port     : process.env.DB_PORT || '3306'
+	host            : process.env.DB_HOST || 'localhost',
+	user            : process.env.DB_USER || 'root',
+	password        : process.env.DB_PASSWORD || 'root',
+	database        : process.env.DB_DATABASE_NAME || 'csr_db',
+	port            : process.env.DB_PORT || '3306',
+	connectionLimit : 20
 });
 console.log('Hostname: ' + process.env.DB_HOST);
 console.log('Username: ' + process.env.DB_USER);
--- api/routes/get/getter.js
+++ api/routes/get/getter.js
@@ -5,13 +5,21 @@ var pool = require('../database').connection;
 router.post('/', function (req, res) {
 	console.log('\x1b[34m', `SELECT ${req.body.select ? req.body.select : '*'} FROM ${req.body.tableName} ${req.body.selectiveGet ? req.body.selectiveGet : ''}`, '\x1b[0m');
 	pool.getConnection(function (err, connection) {
+		if (err) {
+			console.log(err);
+			connection.release();
+			return res.send(400, "Couldn't get a connection");
+		}
 		connection.query(`SELECT ${req.body.select ? req.body.select : '*'} FROM ${req.body.tableName} ${req.body.selectiveGet}`, function (err, result, fields) {
-			if (err) res.status(500).send(err);
+			if (err) {
+				console.log(err);
+				connection.release();
+				res.status(500).send(err);
+			}
 			res.status(200).send(result);
 			console.log(`${req.body.tableName}`, '\x1b[32m', `(sent)`, '\x1b[0m');
 		});
 		connection.release();
-		if (err) console.log(err);
 	});
 });
 
