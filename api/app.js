c8be14b0c88b6361d32bf6a0b754d507ed3dd095
Philipp Braun <philipp.braun@telekom.de>
2019-10-10 15:30:52 +0200

    more async

--- api/app.js
+++ api/app.js
@@ -42,9 +42,9 @@ i = schedule.scheduleJob('0 5 * * mon', async function MondayJob () {
 	const Presentations = async () => {
 		return await GetNewPresentations();
 	};
-	console.log(Missing);
-	console.log(Presentations);
-	await PickWeeklyPresenters(Missing, Presentations);
+	console.log(await Missing);
+	console.log(await Presentations);
+	await PickWeeklyPresenters(await Missing, await Presentations);
 });
 
 var j = schedule.scheduleJob('30 5 * * *', function DailyCheck () {
