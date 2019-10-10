8a155edce6333f876f0ee09a410269596468a002
Philipp Braun <philipp.braun@telekom.de>
2019-10-10 16:17:41 +0200

    forgot function braces

--- api/routes/chooseRandom.js
+++ api/routes/chooseRandom.js
@@ -198,8 +198,8 @@ async function GetPresentPeople (MissingPeople, NewPresentations) {
 async function PickWeeklyPresenters () {
 	console.log('\x1b[33m', 'Picking Presenters...', '\x1b[0m');
 	//Check if enough people are present, regardless of if they had a presentation last week
-	let MissingPeople = await getMissingPeople;
-	let NewPresentations = await getNewPresentations;
+	let MissingPeople = await getMissingPeople();
+	let NewPresentations = await getNewPresentations();
 	console.log(MissingPeople);
 	await GetPresentPeople(MissingPeople, NewPresentations);
 
