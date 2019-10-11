7b221ca7429106044610dab64ae900157d42306a
Philipp Braun <philipp.braun@telekom.de>
2019-10-11 10:20:10 +0200

    logging

--- webapp/src/methods/checkResetToken.js
+++ webapp/src/methods/checkResetToken.js
@@ -14,6 +14,7 @@ export default async function checkResetToken (token) {
 		.then((response) => response.json())
 		.then((res) => {
 			const response = res;
+			console.dir(response);
 			return response;
 		});
 }
