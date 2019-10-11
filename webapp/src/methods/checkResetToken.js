bc8250618fcb58a5a7c26cecad1ed00a08f28cce
Philipp Braun <philipp.braun@telekom.de>
2019-10-11 11:17:13 +0200

    trying reject

--- webapp/src/methods/checkResetToken.js
+++ webapp/src/methods/checkResetToken.js
@@ -1,18 +1,23 @@
 import API_URL from '../variables';
 
 export default async function checkResetToken (token) {
-	await fetch(API_URL + '/checkResetToken', {
-		method  : 'POST',
-		headers : {
-			'Content-Type' : 'application/json'
-		},
-		body    : JSON.stringify({
-			username : sessionStorage.getItem('username'),
-			token    : token
-		})
-	}).then((res) => {
-		const response = res;
-		console.dir(response.status);
-		return response.status;
+	return new Promise(function (resolve, reject) {
+		fetch(API_URL + '/checkResetToken', {
+			method  : 'POST',
+			headers : {
+				'Content-Type' : 'application/json'
+			},
+			body    : JSON.stringify({
+				username : sessionStorage.getItem('username'),
+				token    : token
+			})
+		}).then((res) => {
+			const response = res;
+			if (response.status === 200) {
+				resolve(response.status);
+			} else {
+				reject('404');
+			}
+		});
 	});
 }
