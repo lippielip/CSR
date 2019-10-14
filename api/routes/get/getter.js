3166091e1ff8a240cadebcafedda023886fe3e61
Philipp Braun <philipp.braun@telekom.de>
2019-10-14 09:27:57 +0200

    checktoken check

--- api/routes/get/getter.js
+++ api/routes/get/getter.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../database');
+var checkToken = require('../authentication/checkToken');
 // simple multipurpose function for fetching data
 router.post('/', function (req, res) {
+	console.log('token' + checkToken());
 	console.log('\x1b[34m', `SELECT ${req.body.select ? req.body.select : '*'} FROM ${req.body.tableName} ${req.body.selectiveGet ? req.body.selectiveGet : ''}`, '\x1b[0m');
 	pool.getConnection(function (err, connection) {
 		if (err) {
