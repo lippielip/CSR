5aaf166120c90aeb790c6e37697778ed06a466b3
Philipp Braun <philipp.braun@telekom.de>
2019-10-14 09:49:26 +0200

    testing

--- api/routes/authentication/checkTokenInternal.js
+++ api/routes/authentication/checkTokenInternal.js
@@ -2,40 +2,42 @@ var pool = require('../database');
 
 //function to compare the users local token with serverside token
 async function checkTokenInternal (req) {
-	console.log(req.body);
-	console.log(`\x1b[36mChecking Token...\x1b[0m`);
-	pool.getConnection(function (err, connection) {
-		if (err) {
-			console.log(err);
-			return -1;
-		}
-		if (err) {
-			console.log(err);
-			return -1;
-		}
-		connection.query(`Select token, Authentication_Level, Pending_Presentation FROM users WHERE Username = '${req.body.username}' `, function (err, result, fields) {
+	return new Promise(function (resolve, reject) {
+		console.log(req.body);
+		console.log(`\x1b[36mChecking Token...\x1b[0m`);
+		pool.getConnection(function (err, connection) {
 			if (err) {
 				console.log(err);
-				connection.release();
-				return -1;
+				reject(-1);
 			}
-			if (result.length === 0) {
-				// non existent user
-				console.log('\x1b[31mFailed!\x1b[0m');
-				console.log(0);
-				return 0;
+			if (err) {
+				console.log(err);
+				reject(-1);
 			}
-			if (req.body.token === result[0].token) {
-				// successfull authentication
-				console.log('\x1b[32mSuccess!\x1b[0m');
-				return 1;
-			} else {
-				console.log('\x1b[31mFailed!\x1b[0m');
-				console.log(0);
-				return 0;
-			}
+			connection.query(`Select token, Authentication_Level, Pending_Presentation FROM users WHERE Username = '${req.body.username}' `, function (err, result, fields) {
+				if (err) {
+					console.log(err);
+					connection.release();
+					resolve(-1);
+				}
+				if (result.length === 0) {
+					// non existent user
+					console.log('\x1b[31mFailed!\x1b[0m');
+					console.log(0);
+					resolve(0);
+				}
+				if (req.body.token === result[0].token) {
+					// successfull authentication
+					console.log('\x1b[32mSuccess!\x1b[0m');
+					resolve(1);
+				} else {
+					console.log('\x1b[31mFailed!\x1b[0m');
+					console.log(0);
+					resolve(0);
+				}
+			});
+			connection.release();
 		});
-		connection.release();
 	});
 }
 
