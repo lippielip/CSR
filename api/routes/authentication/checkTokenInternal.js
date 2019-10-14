e11f326396ddb6d50ed9c779a31f506fc81ec426
Philipp Braun <philipp.braun@telekom.de>
2019-10-14 15:09:08 +0200

    added returns to end

--- api/routes/authentication/checkTokenInternal.js
+++ api/routes/authentication/checkTokenInternal.js
@@ -6,36 +6,43 @@ async function checkTokenInternal (req) {
 		console.log(`\x1b[36mChecking Token...\x1b[0m`);
 		if (req.body.username === null || req.body.token === null) {
 			reject(-1);
+			return;
 		}
 		pool.getConnection(function (err, connection) {
 			if (err) {
 				console.log(err);
 				reject(-1);
+				return;
 			}
 			if (err) {
 				console.log(err);
 				reject(-1);
+				return;
 			}
 			connection.query(`Select token, Authentication_Level, Pending_Presentation FROM users WHERE Username = '${req.body.username}' `, function (err, result, fields) {
 				if (err) {
 					console.log(err);
 					connection.release();
 					resolve(-1);
+					return;
 				}
 				if (result.length === 0) {
 					// non existent user
 					console.log('\x1b[31mFailed!\x1b[0m');
 					console.log(0);
 					resolve(0);
+					return;
 				}
 				if (req.body.token === result[0].token) {
 					// successfull authentication
 					console.log('\x1b[32mSuccess!\x1b[0m');
 					resolve(1);
+					return;
 				} else {
 					console.log('\x1b[31mFailed!\x1b[0m');
 					console.log(0);
 					resolve(0);
+					return;
 				}
 			});
 			connection.release();
