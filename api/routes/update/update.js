864dff7aaef3c095ec26438b8195902adee37184
Philipp Braun <philipp.braun@telekom.de>
2019-10-16 15:25:12 +0200

    returned to old config (stable)

--- api/routes/update/update.js
+++ api/routes/update/update.js
@@ -6,7 +6,9 @@ var checkToken = require('../authentication/checkTokenInternal');
 // function to change values
 router.post('/', async function (req, res, next) {
 	if ((await checkToken(req)) >= 5) {
-		pool.getConnection(async function (err, connection) {
+		delete req.body.username;
+		delete req.body.token;
+		pool.getConnection(function (err, connection) {
 			if (err) {
 				console.log(err);
 				return res.status(400).send("Couldn't get a connection");
@@ -14,47 +16,70 @@ router.post('/', async function (req, res, next) {
 			//loop through all categories
 			var category = Object.keys(req.body)[2];
 			if (category === 'presentations') {
-				async function testLength (Date) {
-					return new Promise(function (resolve, reject) {
-						connection.query(`SELECT Date FROM presentations WHERE Date = '${Date}'`, function (err, result, fields) {
-							if (err) console.log(err);
-							if (result.length === 2) {
-								res.status(304).send();
-								connection.release();
-								resolve();
-							} else {
-								resolve();
-							}
-						});
+				connection.query(`SELECT Date FROM presentations WHERE Date = '${req.body.presentations.Date}'`, function (err, result, fields) {
+					if (err) console.log(err);
+					if (result.length === 2) {
+						res.status(304).send();
+						connection.release();
+					} else {
+						var proplength = Object.values(req.body[category]).length; // category amount
+						console.log('\x1b[31m', `Property Amount for Category ${category}: ${proplength}`, '\x1b[0m');
+						for (let j = 0; j < proplength; j++) {
+							// loop through all properties
+							values = Object.values(req.body[category])[j];
+							keys = Object.keys(req.body[category])[j];
+							console.log(
+								'\x1b[34m',
+								`UPDATE`,
+								'\x1b[0m',
+								` ${category}`,
+								'\x1b[32m',
+								`SET`,
+								'\x1b[0m',
+								`${keys} = '${values}'`,
+								'\x1b[32m',
+								`WHERE`,
+								'\x1b[0m',
+								`${req.body.idInfo.idName} = ${req.body.idInfo.id}`
+							);
+							connection.query(`UPDATE ${category} SET ${keys} = '${values}' WHERE ${req.body.idInfo.idName} = ${req.body.idInfo.id} `, function (
+								err,
+								result,
+								fields
+							) {
+								if (err) console.log(err);
+							});
+						}
+						res.status(200).send();
+					}
+				});
+			} else {
+				var proplength = Object.values(req.body[category]).length; // category amount
+				console.log('\x1b[31m', `Property Amount for Category ${category}: ${proplength}`, '\x1b[0m');
+				for (let j = 0; j < proplength; j++) {
+					// loop through all properties
+					values = Object.values(req.body[category])[j];
+					keys = Object.keys(req.body[category])[j];
+					console.log(
+						'\x1b[34m',
+						`UPDATE`,
+						'\x1b[0m',
+						` ${category}`,
+						'\x1b[32m',
+						`SET`,
+						'\x1b[0m',
+						`${keys} = '${values}'`,
+						'\x1b[32m',
+						`WHERE`,
+						'\x1b[0m',
+						`${req.body.idInfo.idName} = ${req.body.idInfo.id}`
+					);
+					connection.query(`UPDATE ${category} SET ${keys} = '${values}' WHERE ${req.body.idInfo.idName} = ${req.body.idInfo.id} `, function (err, result, fields) {
+						if (err) console.log(err);
 					});
 				}
-				await testLength(req.body.presentations.Date);
+				res.status(200).send();
 			}
-			var proplength = Object.values(req.body[category]).length; // category amount
-			console.log('\x1b[31m', `Property Amount for Category ${category}: ${proplength}`, '\x1b[0m');
-			for (let j = 0; j < proplength; j++) {
-				// loop through all properties
-				values = Object.values(req.body[category])[j];
-				keys = Object.keys(req.body[category])[j];
-				console.log(
-					'\x1b[34m',
-					`UPDATE`,
-					'\x1b[0m',
-					` ${category}`,
-					'\x1b[32m',
-					`SET`,
-					'\x1b[0m',
-					`${keys} = '${values}'`,
-					'\x1b[32m',
-					`WHERE`,
-					'\x1b[0m',
-					`${req.body.idInfo.idName} = ${req.body.idInfo.id}`
-				);
-				connection.query(`UPDATE ${category} SET ${keys} = '${values}' WHERE ${req.body.idInfo.idName} = ${req.body.idInfo.id} `, function (err, result, fields) {
-					if (err) console.log(err);
-				});
-			}
-			res.status(200).send();
 			connection.release();
 		});
 	} else {
