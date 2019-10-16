2465d3c671032476f31a572bab4f02e971ee15ac
Philipp Braun <philipp.braun@telekom.de>
2019-10-16 09:52:05 +0200

    testing restructure

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
-import './App.css';
 import Routes from './routes/router';
 
 function App () {
