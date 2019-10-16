881ade0d280a3e1223513e8bbe6e8a3f61a70919
Philipp Braun <philipp.braun@telekom.de>
2019-10-16 10:45:02 +0200

    testing

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes/router';
-require('bootstrap');
+
 function App () {
 	return (
 		<div className="App">
