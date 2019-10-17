fd9269c35d9bacfbbe18a59c141f7e903e8cbee7
Philipp Braun <philipp.braun@telekom.de>
2019-10-17 13:53:33 +0200

    object keys testing

--- api/routes/get/getter.js
+++ api/routes/get/getter.js
@@ -17,7 +17,7 @@ router.post('/', async function (req, res) {
 					connection.release();
 					res.status(500).send(err);
 				}
-				console.log(result.includes('Missing_ID'));
+				console.log(Object.keys(result).includes('Missing_ID'));
 				res.status(200).send(result);
 				console.log(`${req.body.tableName}`, '\x1b[32m', `(sent)`, '\x1b[0m');
 			});
