09116adbde7e191f907ad824cab9ed67b16bb3b7
Philipp Braun <philipp.braun@telekom.de>
2019-10-17 14:57:06 +0200

    test

--- api/routes/get/getter.js
+++ api/routes/get/getter.js
@@ -8,8 +8,7 @@ router.post('/', async function (req, res) {
 	if ((await checkToken(req)) >= 5) {
 		console.log(
 			restrictedGetter.some((el) => {
-				console.log(el);
-				console.log(req.body.select.toUpperCase());
+				console.log(req.body.select.toUpperCase().match(el));
 				if (req.body.select.toUpperCase().match(el) !== null) {
 					return el;
 				}
