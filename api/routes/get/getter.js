958b45f9e2feb3c2633c9658fcb56af35a8d85c2
Philipp Braun <philipp.braun@telekom.de>
2019-10-17 14:14:27 +0200

    select all security

--- api/routes/get/getter.js
+++ api/routes/get/getter.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../database');
 var checkToken = require('../authentication/checkTokenInternal');
-var restrictedGetter = [ 'Password', 'token', 'ResetToken' ];
+var restrictedGetter = [ 'Password', 'token', 'ResetToken', '*' ];
 // simple multipurpose function for fetching data
 router.post('/', async function (req, res) {
 	if ((await checkToken(req)) >= 5) {
@@ -16,14 +16,12 @@ router.post('/', async function (req, res) {
 					console.log(err);
 					return res.status(400).send("Couldn't get a connection");
 				}
-				connection.query(`SELECT ${req.body.select ? req.body.select : '*'} FROM ${req.body.tableName} ${req.body.selectiveGet}`, function (err, result, fields) {
+				connection.query(`SELECT ${req.body.select} FROM ${req.body.tableName} ${req.body.selectiveGet ? req.body.selectiveGet : ''}`, function (err, result, fields) {
 					if (err) {
 						console.log(err);
 						connection.release();
 						res.status(500).send(err);
 					}
-					console.log(Object.keys(result[0]).includes('Topic'));
-
 					res.status(200).send(result);
 					console.log(`${req.body.tableName}`, '\x1b[32m', `(sent)`, '\x1b[0m');
 				});
